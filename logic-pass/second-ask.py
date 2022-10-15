45ca096c82c1c6c83529eed94399b135e3314436
hamzaadel0 <52141498+hamzaadel0@users.noreply.github.com>
2022-10-15 03:01:57 +0300

    Update second-ask.py

--- logic-pass/second-ask.py
+++ logic-pass/second-ask.py
@@ -3,8 +3,7 @@ def prime(start,end):
     for num in range (start,end+1):
     #start end is the range of given numbers
         if start>1 : 
-   ''' A prime number is a whole number greater than 1 whose only factors are
-   1 and itself.'''     
+      
             for i in range(2,num):
                 if num%i==0:
                     break
